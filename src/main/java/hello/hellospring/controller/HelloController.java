70403cf03a98fa2594c530f421be28d62511c81b
ahnjungeun <616a65@gmail.com>
2023-01-06 22:33:59 +0900

    스프링 웹 개발 기초 - API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,32 @@ public class HelloController {
         model.addAttribute("key", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("str") String name) {
+        return "안녕하세요, " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("name") String username) {
+        Hello hello = new Hello();
+        hello.setName(username);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        // generate로 간편하게 생성! (alt + insert)
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }

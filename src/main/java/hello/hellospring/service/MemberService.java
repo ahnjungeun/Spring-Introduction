79afa52a8329adbbd2485f44692d56ea93b2ad6b
ahnjungeun <616a65@gmail.com>
2023-01-11 22:47:47 +0900

    회원 관리 예제 - 백엔드 개발 - 회원 서비스 개발

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,67 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 주석쓰기 /** 치고 엔터
+
+    /**
+     * 회원가입
+     *
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        // 같은 이름은 회원가입이 불가능하게, 중복가입방지
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        // result에 값이 있으면 람다 실행
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        // 개선 1 : Optional로 나오면 코드가 안 예쁨
+        memberRepository.findByName(member.getName()) // 이 값이 Optional이니까 바로 반환해서 사용
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+
+        // 개선 2 : 이런 비즈니스 로직은 메소드로 분리하는 것이 낫다.
+        validateDuplicateMember(member); // 최종
+
+        // 기본 회원가입은 save만 하면 끝
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 개선 2 - 메소드 추출 부분
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한 명 조회
+     * @param memberId
+     * @return
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
